b2fb82a0bc9cedd8a160b8777b80c469aeb7e50e
ratiugtiug <hw45417@horwang.ac.th>
2023-12-05 13:10:19 +0700

    DIP_calculator

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class MyCalculator:
         self.button.place(x=10, y=50)
 
         self.button = tk.Button(self.root, text="+/-", height=1, width=4 ,font=('Arial', 18))
-        self.button.place(x=90, y=50)
+        self.button.place(x=85, y=50)
 
         self.button = tk.Button(self.root, text="%", height=1, width=4 ,font=('Arial', 18))
         self.button.place(x=170, y=50)
@@ -24,7 +24,7 @@ class MyCalculator:
         self.button.place(x=10, y=100)
 
         self.button = tk.Button(self.root, text="8", height=1, width=4 ,font=('Arial', 18))
-        self.button.place(x=90, y=100)
+        self.button.place(x=85, y=100)
 
         self.button = tk.Button(self.root, text="9", height=1, width=4 ,font=('Arial', 18))
         self.button.place(x=170, y=100)
@@ -33,7 +33,7 @@ class MyCalculator:
         self.button.place(x=10, y=150)
 
         self.button = tk.Button(self.root, text="5", height=1, width=4 ,font=('Arial', 18))
-        self.button.place(x=90, y=150)
+        self.button.place(x=85, y=150)
 
         self.button = tk.Button(self.root, text="6", height=1, width=4 ,font=('Arial', 18))
         self.button.place(x=170, y=150)
@@ -42,7 +42,7 @@ class MyCalculator:
         self.button.place(x=10, y=200)
 
         self.button = tk.Button(self.root, text="2", height=1, width=4 ,font=('Arial', 18))
-        self.button.place(x=80, y=200)
+        self.button.place(x=85, y=200)
 
         self.button = tk.Button(self.root, text="3", height=1, width=4 ,font=('Arial', 18))
         self.button.place(x=170, y=200)
